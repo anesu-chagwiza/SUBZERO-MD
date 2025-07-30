ba3de9463fca461d6b356576715ab3e5c8e2b7d4
anesu-chagwiza <archytech834@gmail.com>
2025-07-30 23:55:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // SUBZERO-MD;;;eEkXlIDI#G51upJefXEO2EQXkuBEScrXNZbnBkONpD3CH8dswEJE"*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", //SUBZERO-MD;;;eEkXlIDI#G51upJefXEO2EQXkuBEScrXNZbnBkONpD3CH8dswEJE"*"
+  OWNER_NUMBER: "263771242725", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
